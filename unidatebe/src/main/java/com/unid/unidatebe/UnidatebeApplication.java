0fffa7a0d40524e0630f63179a3526f32a46f45e
Francesco Amato <amato.francesco2000@gmail.com>
2025-04-10 20:47:32 +0200

    initial commit

---
+++ unidatebe/src/main/java/com/unid/unidatebe/UnidatebeApplication.java
@@ -0,0 +1,13 @@
+package com.unid.unidatebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnidatebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnidatebeApplication.class, args);
+	}
+
+}
---
+++ unidatebe/src/test/java/com/unid/unidatebe/UnidatebeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unid.unidatebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidatebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
